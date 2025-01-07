59ddc52f177ed8a2a384f256d2392df76d7ed52e
gerard.park <dev.gerardpark@gmail.com>
2025-01-07 21:30:35 +0900

    test

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ import fs from 'fs';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
